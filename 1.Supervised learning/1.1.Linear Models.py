8f690240a62a830253133b5527a7b1d21fe5844c
jnguyen1192 <johdu13@hotmail.fr>
2020-03-09 11:09:00 +0100

    #3 Linear models introduction

---
+++ 1.Supervised learning/1.1.Linear Models.py
@@ -0,0 +1,2 @@
+print("1.1 Linear Models")
+print("Linear combination of the features with y as the predicted value\n The formula is y(w, x) = w0 + w1x1 + ... + wpxp with w the interception(difference) and x the dataset")

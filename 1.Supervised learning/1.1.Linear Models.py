dabaf6a39a32bb9a7345fc078631f1289912e732
jnguyen1192 <johdu13@hotmail.fr>
2020-03-09 15:23:26 +0100

    #3 1.1.3 Lasso done

--- 1.Supervised learning/1.1.Linear Models.py
+++ 1.Supervised learning/1.1.Linear Models.py
@@ -32,6 +32,19 @@ print("Create a linear model with ridge and cross validation:\n", reg)
 print("Train the linear model with ridge and cross validation:\n", reg.fit([[0, 0], [0, 0], [1, 1]], [0, .1, 1]))
 print("Get the alpha value:\n", reg.alpha_)
 print("It has the same complexity than ordinary least squares (O(n_samples*n²_features))")
+# TODO bonus
+#   print the equation y = ax + b + alpha
+print("-" * 200)
+print("\t"*1 + "1.1.3 Lasso")
+print("Lasso is a linear model that estimate sparse coefficients using L1 norm\nThe formula is :\nmin(w)(1/2*n_samples)||Xw-y||²2 + alpha||w||1\n" + " " * 20 + "with alpha a constant and ||w||1 the L1-norm")
+from sklearn import linear_model
+reg = linear_model.Lasso(alpha=0.1)
+print("Creating linear model with lasso using alpha = 0.1:\n", reg)
+print("Training linear model with lasso using dataset X_train and prediction y_train:\n", reg.fit([[0, 0], [1, 1]], [0, 1]))
+print("Get the score using prediction y_test:\n", reg.predict([[1, 1]]))
+print("\t"*2 + "1.1.3.1 Setting regularization parameter")
+print("For high-dimensional dataset LassoCV is preferable")
+print("For dataset with more sample than features LassoLarsIC is preferable")
 print("-" * 200)
 
 
